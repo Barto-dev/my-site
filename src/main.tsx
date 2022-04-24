9fd01fe70c5bc79d1e33f205bf4ce0931680039f
Illia <illya1004work@gmail.com>
2022-04-24 21:26:30 +0200

    style: styled active nav link

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { render } from 'preact';
 
 import App from './App';
 import './styles/variables.css';
+import './styles/nes.css';
 import './index.css';
 
 render(<App />, document.getElementById('app')!);
--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -7,11 +7,11 @@ const Header = () => {
     <header className={styles.header}>
       <div className="wrapper">
         <nav className={styles.nav} aria-label="Site navigation">
-          <Link className={styles.link} href="/">Home</Link>
-          <Link className={styles.link} href="/works">My works</Link>
-          <Link className={styles.link} href="/technology">Technology</Link>
-          <Link className={styles.link} href="/about">About</Link>
-          <Link className={styles.link} href="/achievements">Achievements</Link>
+          <Link activeClassName="active" className={styles.link} href="/">Home</Link>
+          <Link activeClassName="active" className={styles.link} href="/works">My works</Link>
+          <Link activeClassName="active" className={styles.link} href="/technology">Technology</Link>
+          <Link activeClassName="active" className={styles.link} href="/about">About</Link>
+          <Link activeClassName="active" className={styles.link} href="/achievements">Achievements</Link>
         </nav>
       </div>
     </header>
