6bb8a30ce65a742a797e30e8b20c00c5e98aabaf
Illia <illya1004work@gmail.com>
2022-04-24 19:26:14 +0200

    chore: added path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import * as path from 'path';
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src"),
+    },
+  },
 })
