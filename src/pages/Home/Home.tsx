e9a29180a239e111bc827d0f3bcb47bd4693adc2
Illia <illya1004work@gmail.com>
2022-04-24 18:48:27 +0200

    wip: added Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { FunctionalComponent } from 'preact';
+
+const Home: FunctionalComponent = () => {
+  return (
+    <section>
+      <div className="wrapper">
+        Home
+      </div>
+    </section>
+  );
+};
+
+export default Home;
