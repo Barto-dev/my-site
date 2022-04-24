8f1b1e5696b3fb56afeb714e324cfc3eaacf5fc2
Illia <illya1004work@gmail.com>
2022-04-24 18:48:17 +0200

    wip: added About page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,16 @@
+import { FunctionalComponent } from 'preact';
+
+import styles from './About.module.css';
+
+const About: FunctionalComponent = () => {
+
+  return (
+    <section>
+      <div className="wrapper">
+        about
+      </div>
+    </section>
+  );
+};
+
+export default About;
