e5f7989bf91607ff649f5e3181fdc2a7939a425c
Illia <illya1004work@gmail.com>
2022-04-26 00:14:58 +0200

    wip: added about info and styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,12 +4,20 @@ import cn from 'classnames';
 
 import { nesContainerClass } from '@app/utils/nesStyleClass';
 
+import styles from './Home.module.css';
+
 const Home: FunctionalComponent = () => {
   return (
     <section>
       <div className="wrapper-small">
         <div className={cn(nesContainerClass)}>
-          Home
+          <p className={styles.info}>
+            <span>Hello traveller, glad to see you here, make yourself comfortable and let's get to know each other.</span>
+            <span>My name is Illia. I am a front-end developer and i love to build software</span>
+            <span>I've been a web developer for about three and a half years. During this time I managed to work on many projects in various fields.</span>
+            <span>Constantly interested in everything related to the web platform and Progressive Web Applications.</span>
+            <span>I believe that the difference between the web and the native platform will soon disappear, thanks to projects like Fugu, and the people who come up with all these cool features and implement them in browsers.</span>
+          </p>
         </div>
       </div>
     </section>
