f05941ccddd7ca1c824a2fe40c18d39be2f6d9b6
Illia <illya1004work@gmail.com>
2022-07-03 08:35:18 +0100

    wip: added learn section

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -4,11 +4,21 @@ import cn from 'classnames';
 
 import { nesContainerClass } from '@app/utils/nesStyleClass';
 
+import styles from './Achievments.module.css';
+
 const Achievements: FunctionalComponent = () => {
   return (
     <div className="wrapper-small">
+      <section className={styles.learn}>
+        <div className={cn(nesContainerClass)}>
+          <p>Now I am learning</p>
+          <a href="https://advancedreact.com/">https://advancedreact.com/</a>
+        </div>
+      </section>
+
       <div className={cn(nesContainerClass)}>
-        <p>Achievements</p>
+        <p>Courses, workshops, conferences</p>
+        <a href="https://advancedreact.com/">https://advancedreact.com/</a>
       </div>
     </div>
   );
