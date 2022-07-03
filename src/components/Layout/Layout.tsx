40e1f716941e2bb918759101d1848aa2d644d6be
Illia <illya1004work@gmail.com>
2022-07-03 08:28:08 +0100

    wip: layout style

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,13 @@ import { FunctionalComponent } from 'preact';
 
 import Header from '@app/modules/Header/Header';
 
+import styles from './Layout.module.css';
+
 const Layout: FunctionalComponent = ({ children }) => {
   return (
     <div>
       <Header />
-      <main>
+      <main className={styles.main}>
         {children}
       </main>
     </div>
